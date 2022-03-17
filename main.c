c5d8355bb72d2185207d387daf9b7871132817c1
MosesChan <moseschanx@gmail.com>
2022-03-17 19:04:25 +0800

    added global_counter varibale cirulating monitoring subprogram

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdint.h>
 
+
+uint32_t global_counter = 4;
+
 int main(){
+
+while(1){
+	while(global_counter==0)
+		if(global_counter==0xFFFFFFFF)
+			global_counter=0;
+	global_counter++;
+}
 
 	return 0;
 }
