bd8a75b6f0b207bd9df39afdb4440882f7c8caf1
MosesChan <moseschanx@gmail.com>
2022-03-20 23:50:16 +0800

    main.c complement

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ uint32_t global_counter = 4;
 
 int main(){
 
+	GPIO_MODE_SET(GPIOC,MODE_OUT_10M,CNF_OUT_GP_PUSH,13);
+	GPIO_BIT_RESET(GPIOC,13);
+
 while(1){
 	while(global_counter==0)
 		if(global_counter==0xFFFFFFFF)
