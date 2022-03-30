322b57198a87db6524ceaab1f292065e4bcd2a27
Legacy.Host <moseschanx@gmail.com>
2022-03-30 13:22:29 +0800

    changed _handler to _Handler

--- stm32f103xx_startup.c
+++ stm32f103xx_startup.c
@@ -7,6 +7,7 @@
 
 extern int main(void);
 extern void SystemInit (void);
+extern void TimingDelay_Decrement(void);
 
 extern uint32_t _sdata;
 extern uint32_t _edata;
@@ -16,165 +17,166 @@ extern uint32_t _sbss;
 extern uint32_t _ebss;
 
 // Vector table for other STM32F10xxx devices
-void Reset_handler(void);
+void Reset_Handler(void);
 
-void NMI_handler(void)__attribute__((weak,alias("Default_handler")));
-void HardFault_handler(void)__attribute__((weak,alias("Default_handler")));
-void MemManage_handler(void)__attribute__((weak,alias("Default_handler")));
-void BusFault_handler(void)__attribute__((weak,alias("Default_handler")));
-void UsageFault_handler(void)__attribute__((weak,alias("Default_handler")));
-void SVCall_handler(void)__attribute__((weak,alias("Default_handler")));
-void DebugMonitor_handler(void)__attribute__((weak,alias("Default_handler")));
-void PendSV_handler(void)__attribute__((weak,alias("Default_handler")));
-void SysTick_handler(void)__attribute__((weak,alias("Default_handler")));
-void WWDG_handler(void)__attribute__((weak,alias("Default_handler")));
-void PVD_handler(void)__attribute__((weak,alias("Default_handler")));
-void TAMPER_handler(void)__attribute__((weak,alias("Default_handler")));
-void RTC_handler(void)__attribute__((weak,alias("Default_handler")));
-void FLASH_handler(void)__attribute__((weak,alias("Default_handler")));
-void RCC_handler(void)__attribute__((weak,alias("Default_handler")));
-void EXTI0_handler(void)__attribute__((weak,alias("Default_handler")));
-void EXTI1_handler(void)__attribute__((weak,alias("Default_handler")));
-void EXTI2_handler(void)__attribute__((weak,alias("Default_handler")));
-void EXTI3_handler(void)__attribute__((weak,alias("Default_handler")));
-void EXTI4_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMI_Channel1_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMI_Channel2_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMI_Channel3_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMI_Channel4_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMI_Channel5_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMI_Channel6_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMI_Channel7_handler(void)__attribute__((weak,alias("Default_handler")));
-void ADC1_2_handler(void)__attribute__((weak,alias("Default_handler")));
-void USB_HP_CAN_TX_handler(void)__attribute__((weak,alias("Default_handler")));
-void USB_LP_CAN_RX0_handler(void)__attribute__((weak,alias("Default_handler")));
-void CAN_RX1_handler(void)__attribute__((weak,alias("Default_handler")));
-void CAN_SCE_handler(void)__attribute__((weak,alias("Default_handler")));
-void EXTI9_5_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM1_BRK(void)__attribute__((weak,alias("Default_handler")));
-void TIM1_UP_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM1_TRG_COM_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM1_CC_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM2_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM3_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM4_handler(void)__attribute__((weak,alias("Default_handler")));
-void I2C1_EV_handler(void)__attribute__((weak,alias("Default_handler")));
-void I2C1_ER_handler(void)__attribute__((weak,alias("Default_handler")));
-void I2C2_EV_handler(void)__attribute__((weak,alias("Default_handler")));
-void I2C2_ER_handler(void)__attribute__((weak,alias("Default_handler")));
-void SPI1_handler(void)__attribute__((weak,alias("Default_handler")));
-void SPI2_handler(void)__attribute__((weak,alias("Default_handler")));
-void USART1_handler(void)__attribute__((weak,alias("Default_handler")));
-void USART2_handler(void)__attribute__((weak,alias("Default_handler")));
-void USART3_handler(void)__attribute__((weak,alias("Default_handler")));
-void EXTI15_10_handler(void)__attribute__((weak,alias("Default_handler")));
-void RTCAlarm_handler(void)__attribute__((weak,alias("Default_handler")));
-void USBWakeup_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM8_BRK_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM8_UP_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM8_TRG_COM_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM8_CC_handler(void)__attribute__((weak,alias("Default_handler")));
-void ADC3_handler(void)__attribute__((weak,alias("Default_handler")));
-void FSMC_handler(void)__attribute__((weak,alias("Default_handler")));
-void SDIO_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM5_handler(void)__attribute__((weak,alias("Default_handler")));
-void SPI3_handler(void)__attribute__((weak,alias("Default_handler")));
-void UART4_handler(void)__attribute__((weak,alias("Default_handler")));
-void UART5_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM6_handler(void)__attribute__((weak,alias("Default_handler")));
-void TIM7_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMA2_Channel1_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMA2_Channel2_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMA2_Channel3_handler(void)__attribute__((weak,alias("Default_handler")));
-void DMA2_Channel4_5_handler(void)__attribute__((weak,alias("Default_handler")));
+void NMI_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void HardFault_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void MemManage_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void BusFault_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void UsageFault_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void SVCall_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DebugMonitor_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void PendSV_Handler(void)__attribute__((weak,alias("Default_Handler")));
+//void SysTick_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void SysTick_Handler(void);
+void WWDG_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void PVD_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TAMPER_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void RTC_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void FLASH_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void RCC_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void EXTI0_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void EXTI1_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void EXTI2_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void EXTI3_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void EXTI4_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMI_Channel1_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMI_Channel2_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMI_Channel3_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMI_Channel4_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMI_Channel5_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMI_Channel6_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMI_Channel7_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void ADC1_2_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void USB_HP_CAN_TX_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void USB_LP_CAN_RX0_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void CAN_RX1_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void CAN_SCE_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void EXTI9_5_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM1_BRK(void)__attribute__((weak,alias("Default_Handler")));
+void TIM1_UP_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM1_TRG_COM_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM1_CC_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM2_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM3_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM4_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void I2C1_EV_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void I2C1_ER_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void I2C2_EV_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void I2C2_ER_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void SPI1_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void SPI2_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void USART1_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void USART2_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void USART3_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void EXTI15_10_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void RTCAlarm_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void USBWakeup_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM8_BRK_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM8_UP_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM8_TRG_COM_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM8_CC_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void ADC3_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void FSMC_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void SDIO_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM5_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void SPI3_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void UART4_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void UART5_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM6_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void TIM7_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMA2_Channel1_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMA2_Channel2_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMA2_Channel3_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void DMA2_Channel4_5_Handler(void)__attribute__((weak,alias("Default_Handler")));
 
 uint32_t vector_table[] __attribute__((section(".isr_vector")))= {
 	(uint32_t)STACK_START,
-	(uint32_t)&Reset_handler,
-	(uint32_t)&NMI_handler,
-	(uint32_t)&HardFault_handler,
-	(uint32_t)&MemManage_handler,
-	(uint32_t)&BusFault_handler,
-	(uint32_t)&UsageFault_handler,
+	(uint32_t)&Reset_Handler,
+	(uint32_t)&NMI_Handler,
+	(uint32_t)&HardFault_Handler,
+	(uint32_t)&MemManage_Handler,
+	(uint32_t)&BusFault_Handler,
+	(uint32_t)&UsageFault_Handler,
 	(uint32_t)0,
 	(uint32_t)0,
 	(uint32_t)0,
 	(uint32_t)0,
-	(uint32_t)&SVCall_handler,
-	(uint32_t)&DebugMonitor_handler,
+	(uint32_t)&SVCall_Handler,
+	(uint32_t)&DebugMonitor_Handler,
 	(uint32_t)0,
-	(uint32_t)&PendSV_handler,
-	(uint32_t)&SysTick_handler,
-	(uint32_t)&WWDG_handler,
-	(uint32_t)&PVD_handler,
-	(uint32_t)&TAMPER_handler,
-	(uint32_t)&RTC_handler,
-	(uint32_t)&FLASH_handler,
-	(uint32_t)&RCC_handler,
-	(uint32_t)&EXTI0_handler,
-	(uint32_t)&EXTI1_handler,
-	(uint32_t)&EXTI2_handler,
-	(uint32_t)&EXTI3_handler,
-	(uint32_t)&EXTI4_handler,
-	(uint32_t)&DMI_Channel1_handler,
-	(uint32_t)&DMI_Channel2_handler,
-	(uint32_t)&DMI_Channel3_handler,
-	(uint32_t)&DMI_Channel4_handler,
-	(uint32_t)&DMI_Channel5_handler,
-	(uint32_t)&DMI_Channel6_handler,
-	(uint32_t)&DMI_Channel7_handler,
-	(uint32_t)&ADC1_2_handler,
-	(uint32_t)&USB_HP_CAN_TX_handler,
-	(uint32_t)&USB_LP_CAN_RX0_handler,
-	(uint32_t)&CAN_RX1_handler,
-	(uint32_t)&CAN_SCE_handler,
-	(uint32_t)&EXTI9_5_handler,
+	(uint32_t)&PendSV_Handler,
+	(uint32_t)&SysTick_Handler,
+	(uint32_t)&WWDG_Handler,
+	(uint32_t)&PVD_Handler,
+	(uint32_t)&TAMPER_Handler,
+	(uint32_t)&RTC_Handler,
+	(uint32_t)&FLASH_Handler,
+	(uint32_t)&RCC_Handler,
+	(uint32_t)&EXTI0_Handler,
+	(uint32_t)&EXTI1_Handler,
+	(uint32_t)&EXTI2_Handler,
+	(uint32_t)&EXTI3_Handler,
+	(uint32_t)&EXTI4_Handler,
+	(uint32_t)&DMI_Channel1_Handler,
+	(uint32_t)&DMI_Channel2_Handler,
+	(uint32_t)&DMI_Channel3_Handler,
+	(uint32_t)&DMI_Channel4_Handler,
+	(uint32_t)&DMI_Channel5_Handler,
+	(uint32_t)&DMI_Channel6_Handler,
+	(uint32_t)&DMI_Channel7_Handler,
+	(uint32_t)&ADC1_2_Handler,
+	(uint32_t)&USB_HP_CAN_TX_Handler,
+	(uint32_t)&USB_LP_CAN_RX0_Handler,
+	(uint32_t)&CAN_RX1_Handler,
+	(uint32_t)&CAN_SCE_Handler,
+	(uint32_t)&EXTI9_5_Handler,
 	(uint32_t)&TIM1_BRK,
-	(uint32_t)&TIM1_UP_handler,
-	(uint32_t)&TIM1_TRG_COM_handler,
-	(uint32_t)&TIM1_CC_handler,
-	(uint32_t)&TIM2_handler,
-	(uint32_t)&TIM3_handler,
-	(uint32_t)&TIM4_handler,
-	(uint32_t)&I2C1_EV_handler,
-	(uint32_t)&I2C1_ER_handler,
-	(uint32_t)&I2C2_EV_handler,
-	(uint32_t)&I2C2_ER_handler,
-	(uint32_t)&SPI1_handler,
-	(uint32_t)&SPI2_handler,
-	(uint32_t)&USART1_handler,
-	(uint32_t)&USART2_handler,
-	(uint32_t)&USART3_handler,
-	(uint32_t)&EXTI15_10_handler,
-	(uint32_t)&RTCAlarm_handler,
-	(uint32_t)&USBWakeup_handler,
-	(uint32_t)&TIM8_BRK_handler,
-	(uint32_t)&TIM8_UP_handler,
-	(uint32_t)&TIM8_TRG_COM_handler,
-	(uint32_t)&TIM8_CC_handler,
-	(uint32_t)&ADC3_handler,
-	(uint32_t)&FSMC_handler,
-	(uint32_t)&SDIO_handler,
-	(uint32_t)&TIM5_handler,
-	(uint32_t)&SPI3_handler,
-	(uint32_t)&UART4_handler,
-	(uint32_t)&UART5_handler,
-	(uint32_t)&TIM6_handler,
-	(uint32_t)&TIM7_handler,
-	(uint32_t)&DMA2_Channel1_handler,
-	(uint32_t)&DMA2_Channel2_handler,
-	(uint32_t)&DMA2_Channel3_handler,
-	(uint32_t)&DMA2_Channel4_5_handler
+	(uint32_t)&TIM1_UP_Handler,
+	(uint32_t)&TIM1_TRG_COM_Handler,
+	(uint32_t)&TIM1_CC_Handler,
+	(uint32_t)&TIM2_Handler,
+	(uint32_t)&TIM3_Handler,
+	(uint32_t)&TIM4_Handler,
+	(uint32_t)&I2C1_EV_Handler,
+	(uint32_t)&I2C1_ER_Handler,
+	(uint32_t)&I2C2_EV_Handler,
+	(uint32_t)&I2C2_ER_Handler,
+	(uint32_t)&SPI1_Handler,
+	(uint32_t)&SPI2_Handler,
+	(uint32_t)&USART1_Handler,
+	(uint32_t)&USART2_Handler,
+	(uint32_t)&USART3_Handler,
+	(uint32_t)&EXTI15_10_Handler,
+	(uint32_t)&RTCAlarm_Handler,
+	(uint32_t)&USBWakeup_Handler,
+	(uint32_t)&TIM8_BRK_Handler,
+	(uint32_t)&TIM8_UP_Handler,
+	(uint32_t)&TIM8_TRG_COM_Handler,
+	(uint32_t)&TIM8_CC_Handler,
+	(uint32_t)&ADC3_Handler,
+	(uint32_t)&FSMC_Handler,
+	(uint32_t)&SDIO_Handler,
+	(uint32_t)&TIM5_Handler,
+	(uint32_t)&SPI3_Handler,
+	(uint32_t)&UART4_Handler,
+	(uint32_t)&UART5_Handler,
+	(uint32_t)&TIM6_Handler,
+	(uint32_t)&TIM7_Handler,
+	(uint32_t)&DMA2_Channel1_Handler,
+	(uint32_t)&DMA2_Channel2_Handler,
+	(uint32_t)&DMA2_Channel3_Handler,
+	(uint32_t)&DMA2_Channel4_5_Handler
 
 };
 
-void Default_handler(void){
+void Default_Handler(void){
 
 	while(1);
 
 }
 
-void Reset_handler(void){
+void Reset_Handler(void){
 
 	//Initialize .data section 	
 	//copy .data section to SRAM
